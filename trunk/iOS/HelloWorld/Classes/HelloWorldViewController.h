e675741d55377cdb683f9908cee97a3afd6153cf
linder.pascal <linder.pascal@7f4bb86e-033e-e3df-bcf8-27b8b457f78f>
2012-10-28 17:20:43 +0000

    HelloWorld

---
+++ trunk/iOS/HelloWorld/Classes/HelloWorldViewController.h
@@ -0,0 +1,16 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Pascal Linder on 07/05/11.
+//  Copyright 2011 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController {
+
+}
+
+@end
+
