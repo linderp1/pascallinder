7d0b573c13571a6ab9ed358b77ff4c1ea25a8bde
linderp1 <linder.pascal@gmail.com>
2016-04-23 22:46:44 +0200

    retrieved latest versions

--- Arduino/01_Sketches/09_USB_MediaController/USB_MediaController/USB_MediaController.ino
+++ Arduino/01_Sketches/09_USB_MediaController/USB_MediaController/USB_MediaController.ino
@@ -1,14 +1,35 @@
-#include <UsbKeyboard.h>
+//
+// Extension of the initial project downloaded from: http://blog.petrockblock.com/2012/05/19/usb-keyboard-with-arduino-and-v-usb-library-an-example/
+// Adding Volume up/down/mute functions
+// 
+// The following are the added keyboard extension to UsbKeyboard.h library:
+// Key mapping where found in the USB HID library http://www.usb.org/developers/hidpage/Hut1_12v2.pdf
+// --------------------------------------------
+// | Key desc     | Decimal value | Hex value |
+// --------------------------------------------
+// | Mute         |    127        |    7F     |
+// | volume up    |    128        |    80     |
+// | volume down  |    129        |    81     |
+// --------------------------------------------
+//
+// Pascal Linder, September 27th 2015
+//
 
+#include "UsbKeyboard.h"
 
+#define PIN_BUTTON_MUTE 7 // the button KEY_MUTE is attached to pin 7
+#define PIN_BUTTON_VOLUME_UP 8 // the button KEY_VOLUME_UP is attached to pin 8
+#define PIN_BUTTON_VOLUME_DOWN 9 // the button KEY_VOLUME_DOWN is attached to pin 9
 
-#define PIN_BUTTON 7 // the button is attached to pin 7
-
-int lastState = LOW; // LOW is equivalent to 0
+int lastStateBUTTON_MUTE = LOW; // LOW is equivalent to 0
+int lastStateBUTTON_VOLUME_UP = LOW; // LOW is equivalent to 0
+int lastStateBUTTON_VOLUME_DOWN = LOW; // LOW is equivalent to 0
 
 void setup() {
   //  button setup
-  pinMode(PIN_BUTTON, INPUT);
+  pinMode(PIN_BUTTON_MUTE, INPUT);
+  pinMode(PIN_BUTTON_VOLUME_UP, INPUT);
+  pinMode(PIN_BUTTON_VOLUME_DOWN, INPUT);
 
   // USB setup -----------------
   // Disable timer0 since it can mess with the USB timing. Note that
@@ -31,13 +52,36 @@ void loop() {
   UsbKeyboard.update();
 
   // check, if transition to button-pressed has occured
-  if( (digitalRead(PIN_BUTTON) == HIGH) && (lastState==LOW) ) {
-    UsbKeyboard.sendKeyStroke(KEY_A); // if so, send the letter 'a' via USB keyboard
-    lastState = HIGH; // update the state variable
+  if( (digitalRead(PIN_BUTTON_MUTE) == HIGH) && (lastStateBUTTON_MUTE==LOW) ) {
+    //UsbKeyboard.sendKeyStroke(KEY_A); // if so, send the letter 'a' via USB keyboard
+    UsbKeyboard.sendKeyStroke(KEY_MUTE);
+    lastStateBUTTON_MUTE = HIGH; // update the state variable
     delay(10); // simple de-bouncing
   // check, if the button was released
-  } else if( (digitalRead(PIN_BUTTON) == LOW) && (lastState==HIGH) ) {
-    lastState = LOW; // if so, update the state variable
+  } else if( (digitalRead(PIN_BUTTON_MUTE) == LOW) && (lastStateBUTTON_MUTE==HIGH) ) {
+    lastStateBUTTON_MUTE = LOW; // if so, update the state variable
+    delay(10); // simple de-bouncing
+  }
+  
+  if( (digitalRead(PIN_BUTTON_VOLUME_UP) == HIGH) && (lastStateBUTTON_VOLUME_UP==LOW) ) {
+    //UsbKeyboard.sendKeyStroke(KEY_A); // if so, send the letter 'a' via USB keyboard
+    UsbKeyboard.sendKeyStroke(KEY_VOLUME_UP);
+    lastStateBUTTON_VOLUME_UP = HIGH; // update the state variable
+    delay(10); // simple de-bouncing
+  // check, if the button was released
+  } else if( (digitalRead(PIN_BUTTON_VOLUME_UP) == LOW) && (lastStateBUTTON_VOLUME_UP==HIGH) ) {
+    lastStateBUTTON_VOLUME_UP = LOW; // if so, update the state variable
+    delay(10); // simple de-bouncing
+  }
+  
+  if( (digitalRead(PIN_BUTTON_VOLUME_DOWN) == HIGH) && (lastStateBUTTON_VOLUME_DOWN==LOW) ) {
+    //UsbKeyboard.sendKeyStroke(KEY_A); // if so, send the letter 'a' via USB keyboard
+    UsbKeyboard.sendKeyStroke(KEY_VOLUME_DOWN);
+    lastStateBUTTON_VOLUME_DOWN = HIGH; // update the state variable
+    delay(10); // simple de-bouncing
+  // check, if the button was released
+  } else if( (digitalRead(PIN_BUTTON_VOLUME_DOWN) == LOW) && (lastStateBUTTON_VOLUME_DOWN==HIGH) ) {
+    lastStateBUTTON_VOLUME_DOWN = LOW; // if so, update the state variable
     delay(10); // simple de-bouncing
   }
 
