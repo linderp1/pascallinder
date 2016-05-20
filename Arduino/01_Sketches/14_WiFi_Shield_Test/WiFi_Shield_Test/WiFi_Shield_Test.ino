91f22c0fcdd06248afe4d26665a8281ce240f920
linderp1 <linder.pascal@gmail.com>
2016-05-20 19:55:12 +0200

    Added WiFly Shield sketches and libraries

---
+++ Arduino/01_Sketches/14_WiFi_Shield_Test/WiFi_Shield_Test/WiFi_Shield_Test.ino
@@ -0,0 +1,58 @@
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include "WiFly.h"
+
+#define SSID      "Livebox-6A60"
+#define KEY       "5008apt19"
+#define AUTH      WIFLY_AUTH_WPA2_PSK     // or WIFLY_AUTH_WPA1, WIFLY_AUTH_WEP, WIFLY_AUTH_OPEN
+
+// Pins' connection
+// Arduino       WiFly
+//  2    <---->    TX
+//  3    <---->    RX
+SoftwareSerial uart(2, 3);
+WiFly wifly(&uart);
+//WiFly wifly(&Serial1);     // for leonardo, use hardware serial - Serial1
+
+void setup() {
+  uart.begin(9600);
+
+  Serial.begin(9600);
+  Serial.println("--------- WIFLY TEST --------");
+  
+  // wait for initilization of wifly
+  delay(3000);
+  
+  uart.begin(9600);     // WiFly UART Baud Rate: 9600
+  
+  wifly.reset();
+  
+  Serial.println("Join " SSID );
+  if (wifly.join(SSID, KEY, AUTH)) {
+    Serial.println("OK");
+  } else {
+    Serial.println("Failed");
+  }
+  
+  // get WiFly params
+  wifly.sendCommand("get everthing\r");
+  char c;
+  while (wifly.receive((uint8_t *)&c, 1, 300) > 0) {
+    Serial.print((char)c);
+  }
+  
+  if (wifly.commandMode()) {
+    Serial.println("Enter command mode. Send \"exit\"(with \\r) to exit command mode");
+  }
+}
+
+void loop() {
+  while (wifly.available()) {
+    Serial.write(wifly.read());
+  }
+  
+  while (Serial.available()) {
+    wifly.write(Serial.read());
+  }
+}
